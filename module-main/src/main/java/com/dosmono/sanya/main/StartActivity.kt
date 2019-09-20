5271b87f715d57e7fafbbe8019de60d9c2e70146
sunze <wosikasun@gmail.com>
2019-09-20 09:43:42 +0800

    将宿主完全空壳化。

--- module-main/src/main/java/com/dosmono/sanya/main/StartActivity.kt
+++ module-main/src/main/java/com/dosmono/sanya/main/StartActivity.kt
@@ -1,16 +1,16 @@
-package com.dosmono.sanya.component
+package com.dosmono.sanya.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alibaba.android.arouter.launcher.ARouter
 import com.dosmono.sanya.base.RouterParty
-import kotlinx.android.synthetic.main.activity_start.*
+import kotlinx.android.synthetic.main.main_activity_start.*
 
 class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.main_activity_start)
 
         btn.setOnClickListener {
 
