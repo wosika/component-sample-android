685d1ff094deba2e52cc2b7334bd4d9d46aa70d0
sunze <wosikasun@gmail.com>
2019-09-20 09:29:51 +0800

    独立运行每个module支持完成。

---
+++ lib-base/src/main/java/com/dosmono/sanya/base/App.kt
@@ -0,0 +1,17 @@
+package com.dosmono.sanya.base
+
+import android.app.Application
+import com.alibaba.android.arouter.launcher.ARouter
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {//Timber日志打印
+            ARouter.openLog()     // 打印日志
+            ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+            ARouter.monitorMode()
+        }
+        ARouter.init(this)
+    }
+}
--- module-main/src/main/java/com/dosmono/sanya/main/MainActivity.kt
+++ module-main/src/main/java/com/dosmono/sanya/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.dosmono.sanya.base.RouterParty
+import com.dosmono.sanya.main.R
 import kotlinx.android.synthetic.main.main_activity_main.*
 
 @Route(path = RouterParty.Main.MAIN_ACTIVITY)
